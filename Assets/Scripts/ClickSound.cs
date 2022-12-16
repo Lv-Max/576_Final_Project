62dadc7264e04b086f1fc26823b5a64443a9447d
Vincent Lyu <1048413433@qq.com>
2022-12-15 20:12:03 -0500

    Add Cursor

---
+++ Assets/Scripts/ClickSound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void OnMouseDown()
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
